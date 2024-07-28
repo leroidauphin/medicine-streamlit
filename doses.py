f97e7b76b2d0d9b3b6faeca02cffb34cc87c5071
John Mcleod <johnangusmcleod@gmail.com>
2024-07-28 22:29:01 +0100

    Add summary lines

--- doses.py
+++ doses.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 
 from config import doses_file_path
@@ -16,3 +16,10 @@ def add(people_id, medicine_id):
     )
     new_doses = pd.concat([doses, new_row])
     new_doses.to_csv(doses_file_path, index=False)
+
+
+def doses_last_24hrs():
+    doses = pd.read_csv(doses_file_path, parse_dates=["dose_datetime"])
+    start_timestamp = datetime.now() - timedelta(hours=24)
+    rows_last_24hrs = doses[doses["dose_datetime"] > start_timestamp]
+    return rows_last_24hrs
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import streamlit as st
 
 from config import people_file_path, medicines_file_path
-from doses import add
+from doses import add, doses_last_24hrs
 
 # read static metadata
 people = pd.read_csv(people_file_path)
@@ -25,6 +26,20 @@ with columns[1]:
 with columns[2]:
     st.button("Add", on_click=_add_new_dose, args=(person, medicine))
 
+doses = doses_last_24hrs()
+doses_with_persons = doses.merge(people, left_on="people_id", right_index=True)
+full_df = doses_with_persons.merge(medicines, left_on="medicines_id", right_index=True)
+summary = full_df.groupby(['people_id', 'medicines_id']).agg(['min', 'max', 'size'])
+display_df = full_df.drop(["people_id", "medicines_id"], axis=1)
 
+for i, row in summary.iterrows():
+    person_name = row["name_x"]["max"]
+    medicine_name = row["name_y"]["max"]
 
+    daily_doses_agg = row["max_per_24hrs"]
+    if daily_doses_agg["size"] >= daily_doses_agg["max"]:
+        next_dose = row["dose_datetime"]["min"]
+    elif row["dose_datetime"]["max"] + timedelta(hours=4) >= datetime.now():
+        next_dose = row["dose_datetime"]["max"] + timedelta(hours=4)
 
+    st.text(f"{person_name} next dose of {medicine_name} at {next_dose}.")
